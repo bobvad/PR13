56b910945462866c88bf729b44ef635362791f60
Djfjfj2006@outlook.com <Djfjfj2006@outlook.com>
2025-11-16 15:54:01 +0500

    Разработанные переменные. Дегтянников

--- app/src/main/java/com/example/gps_degtinnikov/MainActivity.java
+++ app/src/main/java/com/example/gps_degtinnikov/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.gps_degtinnikov;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -15,10 +20,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
+LocationManager _LocationManager;
+    int ACCESS_FINE_LOCATION;
+    int ACCESS_COARSE_LOCATION;
+    TextView result;
+    LocationListener _LocationListener = new LocationListener() {
+            @Override
+        public void onLocationChanged(@NonNull Location location) {
+            if (location == null) return;
+            else {
+                String message = "";
+                if(location.getProvider().equals(LocationManager.GPS_PROVIDER))
+                {
+                    message += "\nМестоположение определено с помощью GPS: долгота -" +
+                            location.getLatitude() + "широта -" + location.getLongitude();
+                }
+                if(location.getProvider().equals(LocationManager.NETWORK_PROVIDER))
+                {
+                    message += "\nМестоположение определено с помощью GPS: долгота -" +
+                            location.getLatitude() + "широта -" + location.getLongitude();
+                }
+                result.setText(message);
+            }
+        }
+    };
 }
